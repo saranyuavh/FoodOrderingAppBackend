218232f91f0169104c85aadf5b5fe2bf3c0a61d6
Yaseen <imheart@gmail.com>
2021-05-29 17:20:40 +0300

    Renamed Customer Business Service

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -3,7 +3,7 @@ package com.upgrad.FoodOrderingApp.api.controller;
 
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
-import com.upgrad.FoodOrderingApp.service.businness.CustomerBusinessService;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
 import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Valid;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.util.Set;
@@ -24,7 +23,7 @@ import java.util.UUID;
 @RequestMapping("/customer")
 public class CustomerController {
     @Autowired
-    private CustomerBusinessService customerBusinessService;
+    private CustomerService customerService;
 
     //@Validator and @RequestBody annotation used to validate the request object
     @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -57,7 +56,7 @@ public class CustomerController {
         customerEntity.setPassword(signupUserRequest.getPassword());
         customerEntity.setSalt("1234abc");
         customerEntity.setContactNumber(signupUserRequest.getContactNumber());
-        final CustomerEntity createdUserEntity = customerBusinessService.signup(customerEntity);
+        final CustomerEntity createdUserEntity = customerService.signup(customerEntity);
         SignupCustomerResponse userResponse = new SignupCustomerResponse().id(createdUserEntity.getUuid()).status("CUSTOMER SUCCESSFULLY REGISTERED");
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
