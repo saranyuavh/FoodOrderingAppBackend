10890b9abdf6c6d5a972b67e7bd3f4ec5a3df8bf
saranyuavh <saranyu38@gmail.com>
2021-05-31 13:48:35 +0530

    controller for get restaurants api done

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -1,25 +1,26 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
-import com.upgrad.FoodOrderingApp.api.model.RestaurantList;
-import com.upgrad.FoodOrderingApp.api.model.RestaurantListResponse;
-import com.upgrad.FoodOrderingApp.api.model.UpdateCustomerRequest;
-import com.upgrad.FoodOrderingApp.api.model.UpdateCustomerResponse;
+import com.upgrad.FoodOrderingApp.api.model.*;
 import com.upgrad.FoodOrderingApp.service.businness.AddressService;
 import com.upgrad.FoodOrderingApp.service.businness.RestaurantService;
+import com.upgrad.FoodOrderingApp.service.businness.StateService;
 import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
-import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/")
 public class RestaurantController {
     @Autowired
     private RestaurantService restaurantBusinessService;
@@ -27,8 +28,54 @@ public class RestaurantController {
     @Autowired
     private AddressService addressService;
 
+    @Autowired
+    private StateService stateService;
+
     @RequestMapping(method = RequestMethod.GET, path = "/restaurant", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<RestaurantListResponse> getAllRestaurants() {
-        return new ResponseEntity<RestaurantListResponse>(new RestaurantListResponse(), HttpStatus.OK);
+        final List<RestaurantEntity> allRestaurants = restaurantBusinessService.getAllRestaurants();
+
+        RestaurantListResponse restaurantListResponse = new RestaurantListResponse();
+
+        List<RestaurantList> details = new ArrayList<RestaurantList>();
+        for (RestaurantEntity restaurant: allRestaurants) {
+            RestaurantList detail = new RestaurantList();
+            detail.setId(UUID.fromString(restaurant.getUuid()));
+            detail.setRestaurantName(restaurant.getRestaurantName());
+            detail.setPhotoURL(restaurant.getPhotoUrl());
+            detail.setCustomerRating(restaurant.getCustomerRating());
+            detail.setAveragePrice(restaurant.getAvgPriceForTwo());
+            detail.setNumberCustomersRated(restaurant.getNumCustomersRated());
+
+            AddressEntity addressEntity = addressService.getAddressById(restaurant.getAddress().getId());
+            RestaurantDetailsResponseAddress responseAddress = new RestaurantDetailsResponseAddress();
+
+            responseAddress.setId(UUID.fromString(addressEntity.getUuid()));
+            responseAddress.setFlatBuildingName(addressEntity.getFlatBuildingNumber());
+            responseAddress.setLocality(addressEntity.getLocality());
+            responseAddress.setCity(addressEntity.getCity());
+            responseAddress.setPincode(addressEntity.getPincode());
+
+            StateEntity stateEntity = stateService.getStateById(addressEntity.getState().getId());
+            RestaurantDetailsResponseAddressState responseAddressState = new RestaurantDetailsResponseAddressState();
+
+            responseAddressState.setId(UUID.fromString(stateEntity.getUuid()));
+            responseAddressState.setStateName(stateEntity.getStateName());
+            responseAddress.setState(responseAddressState);
+
+            detail.setAddress(responseAddress);
+
+            List<String> categoriesList = new ArrayList();
+            for (CategoryEntity category :restaurant.getCategoryEntities()) {
+                categoriesList.add(category.getCategoryName());
+            }
+
+            Collections.sort(categoriesList);
+
+            detail.setCategories(String.join(",", categoriesList));
+            restaurantListResponse.addRestaurantsItem(detail);
+        }
+
+        return new ResponseEntity<RestaurantListResponse>(restaurantListResponse, HttpStatus.OK);
     }
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/AddressService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/AddressService.java
@@ -1,4 +1,17 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.AddressDAO;
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+
 public class AddressService {
+    @Autowired
+    private AddressDAO addressDao;
+
+    @Transactional
+    public AddressEntity getAddressById(final Long addressId) {
+        return addressDao.getAddressById(addressId);
+    }
 }
