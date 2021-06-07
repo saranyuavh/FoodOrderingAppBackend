583eb4ffb1e6ce8080a5aeb492b4fdbc265df98b
saranyuavh <saranyu38@gmail.com>
2021-06-07 13:16:55 +0530

    missed changes

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -3,9 +3,11 @@ package com.upgrad.FoodOrderingApp.service.businness;
 import com.upgrad.FoodOrderingApp.service.dao.CategoryDAO;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CategoryService {
 
     @Autowired
